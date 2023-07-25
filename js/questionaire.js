656ffdd96a54750b0cd6bfa410a866cfd4543cdc
TauriR <tauri.ruut@visioncraft.ee>
2023-07-25 14:17:05 +0300

    added input css and fixed scripts

--- js/questionaire.js
+++ js/questionaire.js
@@ -1,27 +1,37 @@
 function showQuestion(questionNumber) {
-  
-    for (let i = 1; i <= 5; i++) {
-        document.getElementById("question" + i).style.display =
-          i === questionNumber ? "block" : "none";
-      }
-    // For Question 2 (creditLimit slider)
-    if (questionNumber === 2) {
-      const creditLimitSlider = document.querySelector('input[name="creditLimit"]');
-      const creditLimitOutput = document.querySelector('output[for="creditLimit"]');
-      creditLimitOutput.textContent = creditLimitSlider.value;
-      creditLimitSlider.addEventListener('input', () => {
-        creditLimitOutput.textContent = creditLimitSlider.value;
-      });
-    }
-  
-    // For Question 3 (loanTerm slider)
-    if (questionNumber === 3) {
-      const loanTermSlider = document.querySelector('input[name="loanTerm"]');
-      const loanTermOutput = document.querySelector('output[for="loanTerm"]');
-      loanTermOutput.textContent = loanTermSlider.value + " years";
-      loanTermSlider.addEventListener('input', () => {
-        loanTermOutput.textContent = loanTermSlider.value + " years";
-      });
-    }
+  for (let i = 1; i <= 5; i++) {
+    document.getElementById("question" + i).style.display =
+      i === questionNumber ? "block" : "none";
   }
-  
+
+  // hide the intro if button is clicked
+  if (questionNumber === 1) {
+    const intro = document.getElementById("intro");
+    intro.style.display = "none";
+  }
+  // For Question 2 (creditLimit slider)
+  if (questionNumber === 2) {
+    const creditLimitSlider = document.querySelector(
+      'input[name="creditLimit"]'
+    );
+    const creditLimitOutput = document.querySelector(
+      'output[for="creditLimit"]'
+    );
+    creditLimitOutput.textContent = creditLimitSlider.value;
+    creditLimitSlider.addEventListener("input", () => {
+      creditLimitOutput.textContent = creditLimitSlider.value;
+    });
+  }
+
+  // For Question 3 (loanTerm slider)
+  if (questionNumber === 3) {
+    document.addEventListener("DOMContentLoaded", function () {
+      const loanTermSlider = document.querySelector('select[name="loanTerm"]');
+      const loanTermOutput = document.querySelector('output[for="loanTerm"]');
+      loanTermOutput.textContent = loanTermSlider.value;
+      loanTermSlider.addEventListener("change", () => {
+        loanTermOutput.textContent = loanTermSlider.value;
+      });
+    });
+  }
+}
--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 function showSummary() {
-    const productType = document.querySelector('input[name="productType"]:checked').value;
-    const creditLimit = parseFloat(document.querySelector('input[name="creditLimit"]').value);
-    const loanTerm = parseFloat(document.querySelector('input[name="loanTerm"]').value);
-    const interestRate = 0.05; // Hard-coded interest rate of 5%
-    const features = [...document.querySelectorAll('input[name="features"]:checked')]
-      .map((input) => input.value)
-      .join(", ");
-    const comments = document.querySelector('textarea[name="comments"]').value;
-  
-    // Calculate the monthly payment using the provided credit limit, loan term, and interest rate
-    const monthlyPayment = calculateMonthlyPayment(creditLimit, loanTerm, interestRate);
-  
-    // Display the summary information
-    document.getElementById("summaryProductType").textContent = productType;
-    document.getElementById("summaryCreditLimit").textContent = creditLimit;
-    document.getElementById("summaryLoanTerm").textContent = loanTerm;
-    document.getElementById("summaryInterestRate").textContent = interestRate + "%";
-    document.getElementById("summaryMonthlyPayment").textContent = "€" + monthlyPayment;
-    document.getElementById("summaryProductFeatures").textContent = features;
-    document.getElementById("summaryComments").textContent = comments;
-  
-    document.getElementById("questions").style.display = "none";
-    document.getElementById("summary").style.display = "block";
-  }
+  const productType = document.querySelector(
+    'input[name="productType"]:checked'
+  ).value;
+  const creditLimit = parseFloat(
+    document.querySelector('input[name="creditLimit"]').value
+  );
+  const loanTerm = parseFloat(
+    document.querySelector('select[name="loanTerm"]').value
+  );
+  const interestRate = 0.05; // Hard-coded interest rate of 5%
+  const features = [
+    ...document.querySelectorAll('input[name="features"]:checked'),
+  ]
+    .map((input) => input.value)
+    .join(", ");
+  const comments = document.querySelector('textarea[name="comments"]').value;
+
+  // Calculate the monthly payment using the provided credit limit, loan term, and interest rate
+  const monthlyPayment = calculateMonthlyPayment(
+    creditLimit,
+    loanTerm,
+    interestRate
+  );
+
+  // Display the summary information
+  document.getElementById("summaryProductType").textContent = productType;
+  document.getElementById("summaryCreditLimit").textContent = creditLimit;
+  document.getElementById("summaryLoanTerm").textContent = loanTerm;
+  document.getElementById("summaryInterestRate").textContent =
+    interestRate + "%";
+  document.getElementById("summaryMonthlyPayment").textContent =
+    "€" + monthlyPayment;
+  document.getElementById("summaryProductFeatures").textContent = features;
+  document.getElementById("summaryComments").textContent = comments;
+
+  document.getElementById("questions").style.display = "none";
+  document.getElementById("summary").style.display = "block";
+}
+// refresh page if clicked on logo
+function refreshPage() {
+  window.location.reload();
+}
